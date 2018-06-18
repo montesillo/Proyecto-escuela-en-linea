fafb4337490a5f45f4bb18b58d0530fdab4ef1c8
Hiram <montesillo25@gmail.com>
2018-06-17 22:25:57 -0500

    Version 1

---
+++ consulta.php
@@ -0,0 +1,73 @@
+<?php
+	include('conexion.php');
+	/**
+	* 
+	*/
+	class Acceso{
+		private $consulta;
+		private $sql;
+		private $abrir;
+		private $cerrar;
+
+		public function __construct($c){
+			$this->consulta = $c;
+		}
+		#Mando el codigo sql a consulta.php
+		public function Consulta(){
+			$db = new Conexion();
+			$this->sql = $db->Ejecutar($this->consulta);
+			$this->cerrar = $db->Desconectar();
+		}
+		#En este se puede usar tanto para insertar registros como para borrar
+		#Solo envio el codigo sql que necesite utilizar
+		public function InsertarSQL(){
+			$this->Consulta();
+			echo 'Registro Incertado';
+			return $this->cerrar;
+		}
+		public function ListadoSQL(){
+			$this->Consulta();
+			while ($reg = mysqli_fetch_array($this->sql)) {
+				#Aqui ponglo lo que quiero imprimir en pantalla
+				#Tambien puedo obtener datos espesificos de la db
+			}
+
+		}
+		public function UsuariosSQL(){
+				
+				$correo = '';
+				$pass = '';
+				$nom = '';
+			$this->Consulta();
+
+			while ($reg = mysqli_fetch_array($this->sql)) {
+				$correo = $reg['correo'];
+				$pass = $reg['password'];
+				$nom = $reg['nombre'];
+				$maes = $reg['id_maestros'];
+				
+			}
+
+			if ($correo == $_SESSION['email'] and $pass == $_SESSION['pass'] ) {
+				setcookie("sesion", $maes, time()+3600);
+				
+			}else{
+				header('location: inicio.php');
+				unser($_COOKIE["sesion"]);
+
+			}
+			return $this->cerrar;
+
+		}
+
+		public function MateriasSQL(){
+			$this->Consulta();
+			while ($reg = mysqli_fetch_array($this->sql)) {
+				$_SESSION['id_materia'] = $reg['id_materia'];
+			}
+			return $this->cerrar;
+		}
+
+
+	}
+?>
---
+++ inicio.php
@@ -0,0 +1,190 @@
+
+
+<?php
+
+session_start();
+if (isset($_POST['inicio'])) {
+	$_SESSION['email']= $_POST['email'];
+	$_SESSION['pass']= $_POST['pass'];
+	$consulta = "select * from maestros where correo= '$_POST[email]'";
+
+include('consulta.php');
+	$base = new Acceso($consulta);
+	$base->UsuariosSQL();
+}
+
+
+
+	if (empty($_COOKIE['sesion'])) {
+		header('location: inicio.html');
+	}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Escuela On Line</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilos/estilo.css">
+	<script type="text/javascript">
+				function estilos(){
+					
+					document.getElementById("botonesPrincipales").style.display= "none";
+					document.getElementById("regresar").style.display= "block";
+
+				}	
+					function ocultar(){
+					document.getElementById("nivel").style.display= "none";
+					document.getElementById("registroMaterias").style.display= "none";
+					document.getElementById("regresar").style.display= "none";
+					document.getElementById("explicaciones").style.display= "none";
+					document.getElementById("nivel").style.display= "none";
+					}
+			</script>
+</head>
+<body>
+	<header>
+			<div class="ancho">
+				<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="#">Nosotros</a></li>
+					<li><a href="#">Contacto</a></li>
+
+				</ul>
+			</nav>
+		</div>
+		<form method="POST" action="inicio.php">
+			<select name="grado" style="display: none" id="nivel">
+				<option value="0">Elije una opción</option>
+				<optgroup label="Primaria">
+					<option value="1">1°</option>
+					<option value="2">2°</option>
+					<option value="3">3°</option>
+					<option value="4">4°</option>
+					<option value="5">5°</option>
+					<option value="6">6°</option>
+				</optgroup>
+				<optgroup label="Secundaria">
+					<option value="7">1°</option>
+					<option value="8">2°</option>
+					<option value="9">3°</option>
+				</optgroup>
+			</select>
+								
+			<div id="botonesPrincipales">
+				<p><input type="submit" name="materias"  value="Registrar Materias" ><br><br></p>
+				<input type="submit" name="ejercicios" value="Registrar Ejercicios"><br><br>
+				<input type="submit" name="ejemplos" value="Registrar Ejemplos"><br><br>
+				<input type="submit" name="examen" value="Registrar Examen"><br><br>
+			</div>
+			<div id="explicaciones" style="display: none">
+				<p>Coloca la url de tú video: <input type="url" name="url" size="40"></p><br><br>
+				
+				<p>Coloca una explicación sobre el tema: </p>
+				<p><textarea rows="10" cols="60" name="tema"></textarea></p>
+			</div>
+			<div id="registroMaterias" style="display: none;">
+				<p>Nombre de la materia: <input type="text" name="mat"></p>
+				<p></p>	
+			</div>
+			<div id="registroEjercicios" style="display: none;">
+				<p></p>
+			</div>
+				
+			<div id="regresar" style="display: none">
+
+			
+				<p><input type="submit" name="regresar"  value="Regresar"><br><br></p>
+			</div>
+
+			
+
+			<?php 
+				if (isset($_POST['materias'])) {
+					echo '<script type="text/javascript">
+					estilos();
+					document.getElementById("nivel").style.display= "block";
+					document.getElementById("registroMaterias").style.display= "block";
+
+					
+					</script>';
+					 echo '<input type=submit name=materiasEnviar value=Guardar >';
+					
+				}
+				if (isset($_POST['ejercicios'])) {
+					echo '<script type="text/javascript">
+					estilos();
+					</script>';
+				}
+				if (isset($_POST['ejemplos'])) {
+
+					
+					echo '<script type="text/javascript">
+					estilos();
+					document.getElementById("explicaciones").style.display= "block";
+
+					</script>';
+					 echo '<input type=submit name=ejemploEnviar value=Guardar >';
+					
+				}
+				if (isset($_POST['examen'])) {
+
+					echo '<script type="text/javascript">
+					estilos();
+					</script>';
+					
+				}
+				if (isset($_POST['regresar'])) {
+					echo '<script type="text/javascript">
+					ocultar();
+					</script>';
+					
+				}
+				if (isset($_POST['ejemploEnviar'])) {
+					$consulta = "insert into explicaciones(url, explicacion) values ('$_POST[url]', '$_POST[tema]')";
+					include('consulta.php');
+					$base = new Acceso($consulta);
+					$base->InsertarSQL();
+					
+					
+				}
+
+					if (isset($_POST['materiasEnviar'])) {
+					$consulta = "insert into materias(materia) values ('$_POST[mat]')";
+					include('consulta.php');
+					$base = new Acceso($consulta);
+					$base->InsertarSQL();
+
+					$consulta = "select * from materias where materia = '$_POST[mat]'";
+					$base = new Acceso($consulta);
+					$base->MateriasSQL();
+					
+					if (isset($_POST['grado'])) {
+						$grado = $_POST['grado'];
+						if ($grado <=6 ) {
+							$nivel = 'Primaria';
+						}else{
+							$nivel = 'Secundaria';
+						}
+
+						$id_maestro = $_COOKIE['sesion'];
+						$id_materia = $_SESSION['id_materia'];
+
+						$consulta = "insert into niveles(grado, nivel, id_materia, id_maestro) values ('$grado', '$nivel', '$id_materia', '$id_maestro')";
+						$base = new Acceso($consulta);
+						$base->InsertarSQL();
+						
+					}
+					
+				}
+			
+			 ?>
+			
+		</form>
+
+		
+	</header>
+</body>
+</html>
